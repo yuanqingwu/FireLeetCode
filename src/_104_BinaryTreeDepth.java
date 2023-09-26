d15af2b23a337169a343caf7b6d16d6289c5f834
yuanqing.wu <wuyuanqing527@gmail.com>
2023-09-27 00:23:40 +0800

    feat:update code

---
+++ src/_104_BinaryTreeDepth.java
@@ -0,0 +1,54 @@
+/**
+ * 给定一个二叉树 root ，返回其最大深度。
+ * 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+ * 
+ * 解题思路：递归，DFS
+ */
+public class _104_BinaryTreeDepth extends BaseSolution {
+
+    public static void main(String[] args) {
+        _104_BinaryTreeDepth depth = new _104_BinaryTreeDepth();
+        depth.run();
+    }
+
+    @Override
+    void solution() {
+
+        // 3,9,20,null,null,15,7
+        TreeNode root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+        int depth = maxDepth(root);
+        println("depth:" + depth);
+        assert depth == 3;
+
+    }
+
+    /**
+     * Definition for a binary tree node.
+     */
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
