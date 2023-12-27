f3468d5521364ab4886c186fa9233be6f30b81ae
yuanqing.wu <wuyuanqing527@gmail.com>
2023-12-27 23:55:41 +0800

    feat:add case.

---
+++ src/_200_NumberOfIslands.java
@@ -0,0 +1,106 @@
+import static org.junit.Assert.assertEquals;
+
+import tag.Array;
+import tag.DFS_DepthFirstSearch;
+
+/**
+ * Given an m x n 2D binary grid grid which represents a map of '1's (land) and
+ * '0's (water), return the number of islands.
+ * 
+ * An island is surrounded by water and is formed by connecting adjacent lands
+ * horizontally or vertically. You may assume all four edges of the grid are all
+ * surrounded by water.
+ * <p>
+ * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+ * 此外，你可以假设该网格的四条边均被水包围。
+ * 
+ * <p>
+ * Example 1:
+ * 
+ * Input: grid = [
+ * ["1","1","1","1","0"],
+ * ["1","1","0","1","0"],
+ * ["1","1","0","0","0"],
+ * ["0","0","0","0","0"]
+ * ]
+ * Output: 1
+ * <p>
+ * Example 2:
+ * 
+ * Input: grid = [
+ * ["1","1","0","0","0"],
+ * ["1","1","0","0","0"],
+ * ["0","0","1","0","0"],
+ * ["0","0","0","1","1"]
+ * ]
+ * Output: 3
+ * <p>
+ * Constraints:
+ * 
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 300
+ * grid[i][j] is '0' or '1'.
+ */
+@DFS_DepthFirstSearch
+@Array
+public class _200_NumberOfIslands extends BaseSolution {
+
+    public static void main(String[] args) {
+        _200_NumberOfIslands numberOfIslands = new _200_NumberOfIslands();
+        numberOfIslands.run();
+    }
+
+    @Override
+    void solution() {
+
+        char[][] grid = {
+                { '1', '1', '0', '0', '0' },
+                { '1', '1', '0', '0', '0' },
+                { '0', '0', '1', '0', '0' },
+                { '0', '0', '0', '1', '1' }
+        };
+        assertEquals(3, numIslands(grid));
+    }
+
+    public int numIslands(char[][] grid) {
+
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    /**
+     * 好文阅读：
+     * https://leetcode.cn/problems/number-of-islands/solutions/211211/dao-yu-lei-wen-ti-de-tong-yong-jie-fa-dfs-bian-li-
+     * 
+     */
+    public void dfs(char[][] grid, int r, int c) {
+
+        if (!isInArea(grid, r, c)) {
+            return;
+        }
+        if (grid[r][c] != '1') {
+            return;
+        }
+        grid[r][c] = '2';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
+    public boolean isInArea(char[][] grid, int r, int c) {
+        return r >= 0 && r < grid.length
+                && c >= 0 && c < grid[0].length;
+    }
+
+}
