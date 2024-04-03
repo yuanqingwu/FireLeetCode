b2c03c3ecc9c39d1aa9a865beed5ef3189ffc033
yuanqing.wu <wuyuanqing527@gmail.com>
2024-04-03 23:55:57 +0800

    feat:opt code and comment.

--- src/cases/_101_SymmetricTree.java
+++ src/cases/_101_SymmetricTree.java
@@ -2,6 +2,9 @@ package cases;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import basic.tree.binarytree.TreeNode;
 import tag.BFS_BreadthFirstSearch;
 import tag.DFS_DepthFirstSearch;
@@ -9,19 +12,29 @@ import tag.Recursion;
 import tag.Tag_Tree;
 
 /**
+ * [101. Symmetric Tree 对称二叉树](https://leetcode.cn/problems/symmetric-tree)
+ * <p>
  * Given the root of a binary tree, check whether it is a mirror of itself
  * (i.e., symmetric around its center).
  * <p>
- * 给你一个二叉树的根节点 root ， 检查它是否轴对称。
+ * 给你一个二叉树的根节点 root, 检查它是否轴对称。
  * <p>
  * Example 1:
+ * <p>
  * Input: root = [1,2,2,3,4,4,3]
  * Output: true
  * <p>
+ * Example 2:
+ * <p>
+ * Input: root = [1,2,2,null,3,null,3]
+ * Output: false
+ * <p>
  * Constraints:
- * 
+ * <p>
  * The number of nodes in the tree is in the range [1, 1000].
  * -100 <= Node.val <= 100
+ * <p>
+ * Follow up: Could you solve it both recursively and iteratively?
  */
 @Recursion
 @Tag_Tree
@@ -38,6 +51,7 @@ public class _101_SymmetricTree extends BaseSolution {
     void solution() {
         TreeNode root = TreeNode.gen(new int[] { 1, 2, 2, 3, 4, 4, 3 }, 0);
         assertTrue(isSymmetric(root));
+        assertTrue(isSymmetric1(root));
     }
 
     @Recursion(timeComplexity = "O(N)", spaceComplexity = "O(N)")
@@ -61,6 +75,38 @@ public class _101_SymmetricTree extends BaseSolution {
         }
 
         return dfs(root1.left, root2.right) && dfs(root1.right, root2.left);
+    }
+
+    public boolean isSymmetric1(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return bfs(root);
+    }
+
+    @BFS_BreadthFirstSearch(timeComplexity = "O(N)", spaceComplexity = "O(N)")
+    public boolean bfs(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+        while (queue.size() > 0) {
+            TreeNode start = queue.poll();
+            TreeNode end = queue.poll();
+            if (start == null && end == null) {
+                continue;
+            }
+            if (start == null || end == null) {
+                return false;
+            }
+            if (start.val != end.val) {
+                return false;
+            }
+            queue.offer(start.left);
+            queue.offer(end.right);
+            queue.offer(start.right);
+            queue.offer(end.left);
+        }
+        return true;
     }
 
 }
